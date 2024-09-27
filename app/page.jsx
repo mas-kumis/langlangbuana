5ab067b94a8ffc0bcefb45cdf6dbc963c3c3860e
Mas Kumis <wildanblitar@gmail.com>
2024-09-27 21:25:29 +0700

    lalangbuana travel web

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="mt-4 px-[100px]">
+      Hello World
+      <Navbar />
+    </div>
+  );
+}
--- backend/middlewares/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((error) => {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  });
-};
-export default asyncHandler;
