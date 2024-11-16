684428d2099ce18c7631d8262111bd8527996be5
Mas Kumis <wildanblitar@gmail.com>
2024-11-16 23:16:12 +0700

    small update

--- app/page.tsx
+++ app/page.tsx
@@ -111,7 +111,7 @@ export default async function Home() {
             <Card key={i} title={item.title} price={item.price} index={i} />
           ))
         ) : (
-          <p>No promos.</p>
+          <p>No promos Availbale.</p>
         )}
       </AllProduct>
       {/* Jogja */}
